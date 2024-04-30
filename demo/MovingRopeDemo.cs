7fa861ab873f9ae6f247227bf36e9405b38e1ef6
Tshmofen <timohatimafeikq@gmail.com>
2024-05-01 01:35:55 +0300

    Add incoming collisions handler, add new gif

--- demo/MovingRopeDemo.cs
+++ demo/MovingRopeDemo.cs
@@ -4,39 +4,56 @@ namespace VerletRope4;
 
 public partial class MovingRopeDemo : Node3D
 {
-    private bool _moveForward;
-    private Vector3 _targetPosition;
+	private bool _moveForward;
+	private Vector3 _targetPosition;
+    private bool _handleMovingManually;
 
-    [Export] public VerletRope MovingRope { get; set; }
-    [Export] public Vector3 MovingPath { get; set; }
-    [Export] public float PathTime{ get; set; }
-    [Export] public Tween.TransitionType TransitionType { get; set; }
+[Export] public Node3D MovingNode { get; set; }
+	[Export] public Vector3 MovingPath { get; set; }
+	[Export] public float PathTime{ get; set; }
+	[Export] public Tween.TransitionType TransitionType { get; set; }
 
-    private void SynchronizeRopePosition(double _)
+	private void SynchronizeNodePosition(double _)
+	{
+		MovingNode.GlobalPosition = _targetPosition;
+	}
+
+	private void UpdateTargetRopePosition(Vector3 position)
+	{
+		_targetPosition = position;
+	}
+
+	private void MoveNode()
+	{
+		_moveForward = !_moveForward;
+		var sign = _moveForward ? 1 : -1;
+		var fromPosition = MovingNode.GlobalPosition;
+		var toPosition = MovingNode.GlobalPosition + sign * MovingPath;
+
+		var tween = MovingNode.CreateTween().SetTrans(TransitionType).SetProcessMode(Tween.TweenProcessMode.Physics);
+		tween.TweenMethod(Callable.From<Vector3>(UpdateTargetRopePosition), fromPosition, toPosition, PathTime);
+		tween.TweenCallback(Callable.From(MoveNode));
+	}
+
+	public override void _Ready()
+	{
+        if (MovingNode is VerletRope rope)
+        {
+            rope.SimulationStep += SynchronizeNodePosition;
+        }
+		else
+        {
+            _handleMovingManually = true;
+        }
+
+		MoveNode();
+	}
+
+    public override void _Process(double delta)
     {
-        MovingRope.GlobalPosition = _targetPosition;
-    }
-
-    private void UpdateTargetRopePosition(Vector3 position)
-    {
-        _targetPosition = position;
-    }
-
-    private void MoveRope()
-    {
-        _moveForward = !_moveForward;
-        var sign = _moveForward ? 1 : -1;
-        var fromPosition = MovingRope.GlobalPosition;
-        var toPosition = MovingRope.GlobalPosition + sign * MovingPath;
-
-        var tween = MovingRope.CreateTween().SetTrans(TransitionType).SetProcessMode(Tween.TweenProcessMode.Physics);
-        tween.TweenMethod(Callable.From<Vector3>(UpdateTargetRopePosition), fromPosition, toPosition, PathTime);
-        tween.TweenCallback(Callable.From(MoveRope));
-    }
-
-    public override void _Ready()
-    {
-        MovingRope.SimulationStep += SynchronizeRopePosition;
-        MoveRope();
+        if (_handleMovingManually)
+        {
+            SynchronizeNodePosition(delta);
+        }
     }
 }
