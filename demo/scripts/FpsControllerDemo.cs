216b9e776b9e44c59a27c0c5b9fc0edc334219b9
Tshmofen <timohatimafeikq@gmail.com>
2025-09-05 14:14:15 +0300

    Add fishing rod demo

---
+++ demo/scripts/FpsControllerDemo.cs
@@ -0,0 +1,72 @@
+﻿using Godot;
+
+namespace VerletRope.demo.scripts;
+
+public partial class FpsControllerDemo : CharacterBody3D
+{
+    [Export] public float Speed = 5.0f;
+    [Export] public float JumpVelocity = 4.5f;
+    [Export] public float MouseSensitivity = 0.002f;
+    [Export] public float Gravity = 9.8f;
+
+    private Camera3D _camera;
+
+    public override void _Ready()
+    {
+        _camera = GetNode<Camera3D>("Camera3D");
+        Input.MouseMode = Input.MouseModeEnum.Captured; // Lock mouse to screen
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion mouseEvent && Input.MouseMode == Input.MouseModeEnum.Captured)
+        {
+            RotateY(-mouseEvent.Relative.X * MouseSensitivity);
+            _camera.RotateX(-mouseEvent.Relative.Y * MouseSensitivity);
+            _camera.Rotation = new Vector3(
+                Mathf.Clamp(_camera.Rotation.X, -Mathf.Pi / 2.0f, Mathf.Pi / 2.0f),
+                _camera.Rotation.Y,
+                _camera.Rotation.Z
+            );
+        }
+
+        if (@event is InputEventKey { Keycode: Key.Escape, Pressed: true, Echo: false })
+        {
+            Input.MouseMode = Input.MouseMode == Input.MouseModeEnum.Captured 
+                ? Input.MouseModeEnum.Visible 
+                : Input.MouseModeEnum.Captured;
+        }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var velocity = Velocity;
+
+        if (!IsOnFloor())
+        {
+            velocity.Y -= Gravity * (float)delta;
+        }
+
+        if (Input.IsActionJustPressed("jump") && IsOnFloor())
+        {
+            velocity.Y = JumpVelocity;
+        }
+
+        var inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+        var direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+            
+        if (direction != Vector3.Zero)
+        {
+            velocity.X = direction.X * Speed;
+            velocity.Z = direction.Z * Speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+        }
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
+}
